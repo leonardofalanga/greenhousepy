b7900e1d8d0a006502b92e863ccb696868d2724a
leonardofalanga <info@leonardofalanga.it>
2024-11-19 16:46:22 +0100

    Red phase use story 1

--- test/test_greenhouse.py
+++ test/test_greenhouse.py
@@ -13,4 +13,4 @@ class TestGreenhouse(TestCase):
     def test_measure_soil_moisture_valid_range(self, mock_moisture_read):
         mock_moisture_read.return_value = 300
         gh= Greenhouse()
-        self.assertEqual(gh.measure_soil_moisture(), 300)o
+        self.assertEqual(gh.measure_soil_moisture(), 300)om
